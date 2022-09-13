886935fa2b59321bfcf7770f1d555d1b70286711
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-09-13 20:20:08 +0530

    learning variables and statement

---
+++ fundamentals/variables/index.js
@@ -0,0 +1,16 @@
+// varibale - most basic building block
+// variables - Store , Access, Modify === value
+// Declare , Assignment Operator , Assign value
+
+console.log('jagmeet');
+//somecode
+console.log(' jagmeet');
+//somecode
+console.log('jagmeet');
+//somecode
+console.log('jagmeet');
+//somecode
+console.log('jagmeet');
+//somecode
+console.log('jagmeet');
+//somecode
