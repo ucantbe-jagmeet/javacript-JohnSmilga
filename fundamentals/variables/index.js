4d92cfc002df6dcabd36cf5c49a542a47a555e0a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-09-14 20:20:08 +0530

    variables and quatations

--- fundamentals/variables/index.js
+++ fundamentals/variables/index.js
@@ -2,15 +2,20 @@
 // variables - Store , Access, Modify === value
 // Declare , Assignment Operator , Assign value
 
-console.log('jagmeet');
-//somecode
-console.log(' jagmeet');
-//somecode
-console.log('jagmeet');
-//somecode
-console.log('jagmeet');
-//somecode
-console.log('jagmeet');
-//somecode
-console.log('jagmeet');
-//somecode
+/* naming conventions  
+    - can contain digits, letters, underscore, $
+    - must start with letter, $ or -/_
+    
+    no keyword - can not start with number
+
+    -- case sensitive - fullName vs Fullname
+    -- camelCase or _underscore
+*/
+
+let name1 ="jags";
+var value = "Some Value";
+const lastName= "Singh"; // const cant be reasign
+
+console.log(value);
+console.log(name1);
+console.log(lastName);
