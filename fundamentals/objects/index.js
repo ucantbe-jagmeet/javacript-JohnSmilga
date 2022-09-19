fc3b9bf19cfef27d550e4f708533f6891477c083
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-09-19 20:20:10 +0530

    objects

---
+++ fundamentals/objects/index.js
@@ -0,0 +1,24 @@
+// objects - key / value pairs methods
+// dot convention
+
+const person = {
+    name : "Jagmeet",
+    lName : "Singh",
+    age : 22,
+    education : false,
+    married : true,
+    siblings : ['Anna', 'Peter','Susan'],
+    greeting()
+    {
+        // return 'Hello My name is Jags';    
+        console.log('Hello My name is Jags');    
+    },
+};
+
+const age = person.age;
+console.log(age);
+
+console.log(person.greeting());
+// console.log(person.siblings[2]);
+// person.greeting();
+
