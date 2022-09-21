99c51109cd78219edb785b8fe488ad609368af24
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-09-21 20:20:10 +0530

    loops , logival operators and switch statements

---
+++ fundamentals/logicalOperator/index.js
@@ -0,0 +1,15 @@
+// logical operator
+// (|| -> OR) , (&& -> AND), !
+
+const name = 'bobby';
+const age = 23;
+
+if(name != 'bob' && age == 23)
+{
+    console.log('hello there User');
+}
+else
+{
+    console.log('Wrong user');
+}
+
---
+++ fundamentals/loops/index.js
@@ -0,0 +1,37 @@
+// loops 
+// repeatedly run a block of code while condition is true
+// while loop 
+// TURN OFF SAVE
+
+//while loops
+// let amount = 10;
+
+// while(amount > 0)
+// {
+//     console.log('I have ' + amount + ' dollars');
+//     amount--;
+// }
+
+// do while loop -> code blocks first , condiiton second 
+// this runs at least once
+
+// let money = 12;
+// do {
+//     console.log('you have ' + money + ' dollars');
+//     money ++;
+// } while (money <= 10);
+
+// for loops
+// repeatedly run a block of code while condition is true 
+
+// let i;
+
+// for (i = 0; i < 10; i++) 
+// {
+//     console.log('and the number is : ' + i);
+// }
+
+for (let number = 11; number >= 0 ; number--)
+{
+    console.log('and the number is : ' + number); 
+}
