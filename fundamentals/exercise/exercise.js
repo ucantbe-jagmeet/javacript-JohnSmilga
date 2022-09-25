031953f76f6df26e34cba06a71bbf10d6bc5d823
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-09-25 20:20:10 +0530

    exercise on arrays

---
+++ fundamentals/exercise/exercise.js
@@ -0,0 +1,14 @@
+const names = ['anna', 'susy','bob','john'];
+const lastName = 'shakeAndBake';
+
+let newArray = [];
+
+// user for loop to iterate over the array
+for( let i=0; i < names.length; i++)
+{
+    // console.log(names[i]);
+    // newArray.push(names[i]);
+    const fullName = `${names[i]} ${lastName}`;
+    newArray.push(fullName);
+}
+console.log(newArray);
