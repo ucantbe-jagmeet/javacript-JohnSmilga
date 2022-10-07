957746b2a54a6498b7a5d2b3767d2882eb55a11c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-10-07 20:20:10 +0530

    Array Iterator

--- fundamentals/arrayIterator/index.js
+++ fundamentals/arrayIterator/index.js
@@ -1,3 +1,4 @@
 // powerfull Array methods
 // forEach, map, filter, find, reduce
 // Iterate over array - no for loop required
+// Accept CALLBACK function as an argument, call callback against each item in a array. Reference Item in the call back Parameter
