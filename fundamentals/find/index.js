5ae3c697b398ab2d68f3aa29cae505e6f13827ec
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-15 05:38:52 +0530

    find in array

---
+++ fundamentals/find/index.js
@@ -0,0 +1,24 @@
+// find 
+// returns object
+// returns first match, if no match undefined
+// great for getting unique value
+
+
+const people = [
+    { name: 'bob', age: 20, position : 'developer', id:1},
+    { name: 'peter', age: 22, position : 'designer', id:2},
+    { name: 'susy', age: 30, position : 'the Boss', id:3},
+    { name: 'anna', age: 35, position : 'the Boss', id:4},
+];
+
+const names = ['bob', 'peter', 'susy'];
+
+const person = people.find(function(person){
+    return person.id === 3;
+})
+
+
+console.log(name.find( function(name){
+    return name === 'bob'
+}));
+console.log(person);
