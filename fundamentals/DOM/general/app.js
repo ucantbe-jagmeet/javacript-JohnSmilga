ce804611ec2c0329430a3decb506a47fd00074a1
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-21 20:20:10 +0530

    learning DOM manupulation

--- fundamentals/DOM/general/app.js
+++ fundamentals/DOM/general/app.js
@@ -1,5 +1,10 @@
 document.getElementById('btn').style.backgroundColor = 'blue';
 document.body.style.color = 'red';
 document.body.style.backgroundColor = 'yellow';
+// console.log(window);
 
-console.log(window);
+// assign to a variable
+const element = document.getElementById('element');
+
+// do something
+document.querySelector('element');
