7e58f14b19ab77b8cc9fff86a31998ffd959f951
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-22 20:20:10 +0530

    Learning Dom Manupulation

--- fundamentals/DOM/general/app.js
+++ fundamentals/DOM/general/app.js
@@ -8,3 +8,12 @@ const element = document.getElementById('element');
 
 // do something
 document.querySelector('element');
+
+// returns a node object or a node list, which is an arraylike object
+const btn = document.getElementById('btn');
+const name = btn.nodeName;
+const css = btn.style;
+
+console.log(btn);
+console.log(name);
+console.log(css);
