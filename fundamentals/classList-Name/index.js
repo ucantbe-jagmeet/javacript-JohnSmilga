5c33549d39c64bdd9101e02e84143128eb67046b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-12-19 11:59:00 +0530

    learning classList

---
+++ fundamentals/classList-Name/index.js
@@ -0,0 +1,31 @@
+// className
+// classlist
+
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third= document.getElementById('third');
+
+// const classValue = first.className;
+// console.log(classValue);
+
+second.className = "colors text";
+
+
+// third.classList.add('colors')
+// third.classList.add('text')
+third.classList.add('text', 'colors')
+third.classList.remove('text')
+
+
+let result = third.classList.contains('colors')
+if(result)
+{
+    console.log("hello world");
+}
+else{
+    console.log("world hello");
+
+}
+
+const classValue = third.classList;
+console.log(classvalue);
