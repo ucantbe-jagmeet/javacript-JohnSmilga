4e8778b7353ceb82ad5f2f92eefb68049a45382e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-01 21:18:00 +0530

    learning current target

---
+++ fundamentals/currentTarget/index.js
@@ -0,0 +1,31 @@
+// event object argument e , evt 
+// info about triggered event
+// event.type
+// event.currentTarget
+// this keyword
+// preventDefault() - prevents default behaviour
+
+const heading = document.querySelector('h1')
+const btn = document.querySelector('.btn')
+const link = document.getElementById('link')
+
+
+heading.addEventListener('click', function(event){
+    // heading.classList.add('blue')
+    console.log(event.currentTarget);
+    // console.log(this); this also results in same output in console but it is bit confusing 
+})
+
+btn.addEventListener('click', function(event){
+    // heading.classList.add('blue')
+    // console.log(event.currentTarget);
+    event.currentTarget.classList.add('blue');
+    console.log(event.type);
+})
+
+function fun(event){
+    event.preventDefault();
+}
+
+link.addEventListener('click', fun)
+
