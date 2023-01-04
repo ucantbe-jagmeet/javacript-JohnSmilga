ef5ce6791748d2d8161345453a91552c1706ed00
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-04 20:22:37 +0530

    this keyword definition

---
+++ advanced/5-this-keyword/main.js
@@ -0,0 +1,3 @@
+// this keyword --> which is used in a function , refers to the object it belongs to 
+// -> it maeks functions reusable by letting you decide the object value
+// this value is determind entirely by how a function is called
