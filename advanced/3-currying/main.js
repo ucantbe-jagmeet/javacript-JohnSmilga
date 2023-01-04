c2c36900ead47a66b54c6bdb88f7631c44582d19
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-04 20:20:57 +0530

    Curring function

--- advanced/3-currying/main.js
+++ advanced/3-currying/main.js
@@ -5,3 +5,37 @@
 // function f( a, b, c) is transformed to f(a)(b)(c)
 
 // When a function , instead of taking all arguments at one time , takes the first one and return a new function that takes the second one and returns a new functions
+
+
+function sum( a, b, c)
+{
+    return a + b + c;
+}
+
+console.log( sum( 2, 3, 5));
+
+// after currying this function
+
+function curry(fn)
+{
+    return function(a)
+    {
+        return function(b)
+        {
+            return function(c)
+            {
+                 return fn( a, b, c);
+            }
+        }
+    }
+}
+
+const curriedSum = curry(sum);
+console.log( curriedSum(2)(3)(5));
+
+
+const add2 = curriedSum(2);
+const add3 = add2(3);
+const add5 = add3(5);
+
+console.log(add5);
